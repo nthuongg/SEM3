b99216e48af0a1accfa2c5689bf330bf9f260d7b
Nguyen Huong <nguyenhuongg1104@gmail.com>
2023-12-01 09:17:06 +0700

    axios

--- ReactJs/reactjs_demo/src/components/pages/Home.js
+++ ReactJs/reactjs_demo/src/components/pages/Home.js
@@ -1,23 +1,26 @@
+import axios from "axios";
 import React from "react";
 import { Card, Col, Row } from "react-bootstrap";
 
-class HomePage extends React.Component{
+class HomePage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            products : []
+            products: []
         }
     }
 
     componentDidMount() {
         //call api
         const url = `https://dummyjson.com/products?limit=12`;
-        fetch(url).then(rs=>rs.json())
-        .then(rs=>{
-            this.setState({products: rs.products});
-            console.log(rs);
-        })
-        .catch(err=>{console.log(err)});
+        axios.get(url)
+            .then(rs => {
+                //response body của api -> rs.data
+                this.setState({ products: rs.data.products });
+            })
+            .catch(err => { 
+                console.log(err) 
+            });
     }
     render() {
         const products = this.state.products;
@@ -26,25 +29,25 @@ class HomePage extends React.Component{
                 <h1>HomePage</h1>
                 <Row>
                     {products.map(product => (
-                        <Col xs={3}>
-                        <Card.Img src={product.thumbnail}/>
-                        <Card.Body>
-                            <Card.Title>{product.title}</Card.Title>
-                            <Card.Title>{product.description}</Card.Title>
-                        </Card.Body>
-                        <Card.Footer>
-                            <Card.Text>{product.price}</Card.Text>
-                            <Card.Link className="btn btn-primary">
-                                Add to Cart
-                            </Card.Link>
-                        </Card.Footer>
-                    </Col>
+                        <Col xs={3} key={product.id}>
+                            <Card.Img src={product.thumbnail} />
+                            <Card.Body>
+                                <Card.Title>{product.title}</Card.Title>
+                                <Card.Title>{product.description}</Card.Title>
+                            </Card.Body>
+                            <Card.Footer>
+                                <Card.Text>{product.price}</Card.Text>
+                                <Card.Link className="btn btn-primary">
+                                    Add to Cart
+                                </Card.Link>
+                            </Card.Footer>
+                        </Col>
                     ))}
-                    
+
                 </Row>
             </div>
         )
     }
-    
+
 }
 export default HomePage;
